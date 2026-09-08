7452c7825932b475059d2a4c3902ca7c104dc6a3
hhhhvjbh71-web <hhhhvjbh71@gmail.com>
2026-09-08 15:07:55 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Mr. Mostafa Salem
 // ============================================================
 
-const CACHE_VERSION = 'never-give-up-shell-v5';
+const CACHE_VERSION = 'never-give-up-shell-v7';
 const OFFLINE_URL = 'offline.html';
 
 const SHELL_ASSETS = [
